7b54b76dc24c90c95552734345ddcc7f0b3ae757
Skey-11 <sosarosales.k@gmail.com>
2025-12-29 12:24:33 -0600

    cambio de roles

--- src/routes/sales.routes.js
+++ src/routes/sales.routes.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 const controller = require("../controllers/sales.controller");
 const { requireAuth, requireRole } = require("../middlewares/auth.middleware");
 
-router.post("/", requireAuth, requireRole("cashier", "admin", "manager"), controller.create);
+router.post("/", requireAuth, requireRole("cashier", "admin"), controller.create);
 
 module.exports = router;
